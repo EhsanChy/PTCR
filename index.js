f0c982cbd4fd95b8add7085c8f3756310f37c671
unknown <ehsanchy3@gmail.com>
2022-05-28 12:58:07 +0600

    sss

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+// import { createClient } from '@supabase/supabase-js'
+supabase = supabase.createClient('https://ycklrcbhtafbydhxitwl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlja2xyY2JodGFmYnlkaHhpdHdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM3MTYwOTcsImV4cCI6MTk2OTI5MjA5N30.2oXhGKFiwBKyOCxyjypkSXt4Rb7Cq10nTSaYNyGyx0E');
+
+async function onPress(e) {
+    e.preventDefault();
+    var text = document.getElementById('input').value;
+    const { data, error } = await supabase
+        .from('information')
+        .insert([
+            { food_menu: text }
+        ])
+    console.log(data)
+}
